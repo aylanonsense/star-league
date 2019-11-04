068c8970c9ecea742d1322837dd5e596e8ec9a63
Ayla Myers <bridgs.dev@gmail.com>
2019-11-03 19:23:25 -0500

    Adding a reference ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.back * 20 * Time.deltaTime;
+    }
+}
